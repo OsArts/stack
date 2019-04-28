a15d08b3ff6b8bedfbb78e7a4c75fc92cd676c79
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2019-04-28 15:41:56 +0300

    Add: :memo: UI-Kit & april'2019

---
+++ diary/2019/04/apr28.dart
@@ -0,0 +1,10 @@
+// import 'dart:html';
+
+aprDate = MyDate;
+
+MyDate(apr{2019-04-28}, Su){
+  // TODO Describe process creation for Project "PriestCertificate"
+
+}
+
+//- job > webdesigner (UiKit)
